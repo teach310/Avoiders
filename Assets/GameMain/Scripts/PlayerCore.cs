7b3f13539d2fd3557871024b987243da28de8a44
kanekoyuto <kanekoyuuto@gmail.com>
2018-05-05 01:49:19 +0900

    ボールの移動

--- Assets/GameMain/Scripts/PlayerCore.cs
+++ Assets/GameMain/Scripts/PlayerCore.cs
@@ -9,5 +9,22 @@ public class PlayerCore : MonoBehaviour {
 	public GameObject LeftBallGameObj;
 	void Start () {
 		PlayerInput playerInput = GetComponent<PlayerInput>();
+		playerInput.isRightPressDown
+		.DistinctUntilChanged().Subscribe(x => {
+			if(x){
+				RightBallGameObj.transform.position = new Vector3(3, 0.67f, 0);
+			}else{
+				RightBallGameObj.transform.position = new Vector3(1, 0.67f, 0); 
+			}
+		});
+
+		playerInput.isLeftPressDown
+		.DistinctUntilChanged().Subscribe(x => {
+			if(x){
+				LeftBallGameObj.transform.position = new Vector3(-2.66f, 0.67f, 0);
+			}else{
+				LeftBallGameObj.transform.position = new Vector3(-0.92f, 0.67f, 0); 
+			}
+		});
 	}
 }
